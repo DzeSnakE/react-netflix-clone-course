7d11cc16114d772f2ebb8bfc208983fdf71e732e
PierreJean BONAZZI <pierrejean.bonazzi.pj@gmail.com>
2022-02-28 20:19:43 +0100

    Work in progress :)

--- src/components/Header/SigninButton.tsx
+++ src/components/Header/SigninButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 
 /*---> Component <---*/
 const SigninButton = ({ children }: PropsType) => {
